e9f764325f4c973c6dd54ac0a23fb0cf91ef357b
danwang112233 <wmlvbm613@gmail.com>
2017-06-19 23:47:04 +0200

    Update molsetup.py

--- 03-md-phases-412/molsetup.py
+++ 03-md-phases-412/molsetup.py
@@ -1,4 +1,4 @@
-""" This python scripts generates moltemplate files and autogenerates 
+""" This python scripts generates moltemplate files and autogenerates
     data files for atomistic simulations.
 """
 import os
@@ -7,43 +7,49 @@ import subprocess
 import sys
 
 #------------------------------------------------
-if  len(sys.argv) != 5:
-    sys.exit(""" Usage: python molsetup.py <a> <nx> <ny> <nz>
+if  len(sys.argv) != 7:
+    sys.exit(""" Usage: python molsetup.py <a> <n>
                 where `a` (double) : lattice constant ( the simulation box)
                 and `n` (int) : number of replicated RVEs in all direction.
              """)
-else: 
-    a = sys.argv[1]
-    nx = sys.argv[2]
-    ny = sys.argv[3]
-    nz = sys.argv[4] 
-lx = str (float(a) * int(nx))
-ly = str (float(a) * int(ny))
-lz = str (float(a) * int(nz))
+else:
+    a    = float(sys.argv[1])
+    cbya = float(sys.argv[2])
+    nx   =   int(sys.argv[3])
+    ny   =   int(sys.argv[4])
+    nz   =   int(sys.argv[5])
+    m    =   int(sys.argv[6])
+
+lx = a * nx
+ly = a * ny
+lz = cbya * a * nz
 
 raw_txt  = """
-# Generates atom.data which consists of 5n^3 atoms (core+shell=atom). 
+# Generates atom.data which consists of 5*nx*ny*nz atoms (core+shell=atom).
 # This file aids in creation of data files, through moltemplate
-# compatible with read_data in lammps. Data file is according 
-# to the Isotropic-anharmonic Shell Model proposed by Tinte based on 
+# compatible with read_data in lammps. Data file is according
+# to the Isotropic-anharmonic Shell Model proposed by Tinte based on
 # Local Density Approximation(LDA) to Density Functional Theory(DFT):
 # doi:10.1016/j.cossms.2006.05.002 \n
-import "../../commons/molsetup/%s-unit-dyn-vs.lt" \n \n 
-%sCUBIC_BaTiO3_UNIT.scale( %s ) \n
+import "../../commons/molsetup/%s-unit-dyn-vs.lt" \n \n
+%sCUBIC_BaTiO3_UNIT.scale( %f ) \n
 # Periodic boundary conditions:
 write_once("Data Boundary") {
-   0.0  %s xlo xhi
-   0.0  %s ylo yhi
-   0.0  %s zlo zhi
+   0.0  %f xlo xhi
+   0.0  %f ylo yhi
+   0.0  %f zlo zhi
 }
-CUBIC_BaTiO3 = new %sCUBIC_BaTiO3_UNIT[%s].move(  %s, 0.0, 0.0)
-                                      [%s].move( 0.0,  %s, 0.0)
-                                      [%s].move( 0.0, 0.0,  %s)
-"""%( "bt",        "", a, lx, ly, lz,     "", nx, a, ny, a, nz, a )
+CUBIC_BTO {
+CBTO = new %sCUBIC_BaTiO3_UNIT[%d].move(  %f, 0.0, 0.0)
+                              [%d].move( 0.0,  %f, 0.0)
+                              [%d].move( 0.0, 0.0,  %f)
+}
 
+cubic_BaTiO3 = new CUBIC_BTO[%d].move(  %f, 0.0, 0.0)
+"""%( "tb",        "", a, m*lx, ly, lz, "", nx, a, ny, a, nz, a, m, lx )
 
 #-------------------------------------------------CREATE MOLTEMPLATE FILE
-lt_file_name = "atom.lt" 
+lt_file_name = "atom.lt"
 lt_file = open( lt_file_name, "w" )
 lt_file.write("%s" % raw_txt )
 lt_file.close()
