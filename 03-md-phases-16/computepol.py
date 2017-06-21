f042b1266846c79951c333d546a0bf43e799fe5f
danwang112233 <wmlvbm613@gmail.com>
2017-06-21 05:46:11 +0200

    Update computepol.py

--- 03-md-phases-16/computepol.py
+++ 03-md-phases-16/computepol.py
@@ -57,36 +57,39 @@ def pol ( filename, a, alpha, nx, ny, nz ):
         srstr = "c_dr"
     else:
         srstr = "f_AVE_ATOM"
-    for i in range(0, natoms, 10):
-        BaCore  = d.atom( i+ 1, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        BaShell = d.atom( i+ 2, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        TiCore  = d.atom( i+ 3, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        TiShell = d.atom( i+ 4, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        O1Core  = d.atom( i+ 5, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        O1Shell = d.atom( i+ 6, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        O2Core  = d.atom( i+ 7, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        O2Shell = d.atom( i+ 8, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        O3Core  = d.atom( i+ 9, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
-        O3Shell = d.atom( i+10, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+    for i in range(0, nx, 1):
+        for j in range(0, ny, 1):
+            for k in range(0, nz, 1):
+                na = 10*(ny*nz*i+nz*j+k)
+                BaCore  = d.atom( na+ 1, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                BaShell = d.atom( na+ 2, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                TiCore  = d.atom( na+ 3, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                TiShell = d.atom( na+ 4, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                O1Core  = d.atom( na+ 5, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                O1Shell = d.atom( na+ 6, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                O2Core  = d.atom( na+ 7, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                O2Shell = d.atom( na+ 8, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                O3Core  = d.atom( na+ 9, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
+                O3Shell = d.atom( na+10, "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr)
 
-        drBaCore  = np.asarray( list( itertools.chain.from_iterable (BaCore ) ))
-        drBaShell = np.asarray( list( itertools.chain.from_iterable (BaShell) ))
-        drTiCore  = np.asarray( list( itertools.chain.from_iterable (TiCore ) ))
-        drTiShell = np.asarray( list( itertools.chain.from_iterable (TiShell) ))
-        drO1Core  = np.asarray( list( itertools.chain.from_iterable (O1Core ) ))
-        drO1Shell = np.asarray( list( itertools.chain.from_iterable (O1Shell) ))
-        drO2Core  = np.asarray( list( itertools.chain.from_iterable (O2Core ) ))
-        drO2Shell = np.asarray( list( itertools.chain.from_iterable (O2Shell) ))
-        drO3Core  = np.asarray( list( itertools.chain.from_iterable (O3Core ) ))
-        drO3Shell = np.asarray( list( itertools.chain.from_iterable (O3Shell) ))
+                drBaCore  = np.asarray( list( itertools.chain.from_iterable (BaCore ) ))
+                drBaShell = np.asarray( list( itertools.chain.from_iterable (BaShell) ))
+                drTiCore  = np.asarray( list( itertools.chain.from_iterable (TiCore ) ))
+                drTiShell = np.asarray( list( itertools.chain.from_iterable (TiShell) ))
+                drO1Core  = np.asarray( list( itertools.chain.from_iterable (O1Core ) ))
+                drO1Shell = np.asarray( list( itertools.chain.from_iterable (O1Shell) ))
+                drO2Core  = np.asarray( list( itertools.chain.from_iterable (O2Core ) ))
+                drO2Shell = np.asarray( list( itertools.chain.from_iterable (O2Shell) ))
+                drO3Core  = np.asarray( list( itertools.chain.from_iterable (O3Core ) ))
+                drO3Shell = np.asarray( list( itertools.chain.from_iterable (O3Shell) ))
 
-        temp = zBaCore * drBaCore + zBaShell * drBaShell + \
-               zTiCore * drTiCore + zTiShell * drTiShell + \
-               zO1Core * drO1Core + zO1Shell * drO1Shell + \
-               zO2Core * drO2Core + zO2Shell * drO2Shell + \
-               zO3Core * drO3Core + zO3Shell * drO3Shell 
-        temp = 1.60217662 * temp * 10.0 / ( V * nx * ny * nz )
-        p += temp 
+                temp = zBaCore * drBaCore + zBaShell * drBaShell + \
+                       zTiCore * drTiCore + zTiShell * drTiShell + \
+                       zO1Core * drO1Core + zO1Shell * drO1Shell + \
+                       zO2Core * drO2Core + zO2Shell * drO2Shell + \
+                       zO3Core * drO3Core + zO3Shell * drO3Shell 
+                temp = 1.60217662 * temp * 10.0 / ( V * nx * ny * nz )
+                p += temp 
         
     if p[0][0] < 0. and p[0][1] < 0. and p[0][2] < 0.:
         f = -1.
@@ -127,50 +130,3 @@ def find_lat( step, filename):
             print "Timestep not found in the file %s" %filename
     except IOError:
         print "The file %s cannot be accessed or not found"  %filename
-        
-""" Old Code
-    # Born effective charges: Using (dynamic) shell model charges 
-    # [Tinte et al 1999], instead of first principle [Zhong et al 1994]
-    zBa =  2.172
-    zTi =  3.072
-    zO1 = -1.748
-    zO2 = -1.748
-    # Born effect charge tensor
-    ZBa = np.array ( [ [zBa, 0.0, 0.0], [ 0.0, zBa, 0.0 ], [ 0.0, 0.0, zBa] ] )
-    ZTi = np.array ( [ [zTi, 0.0, 0.0], [ 0.0, zTi, 0.0 ], [ 0.0, 0.0, zTi] ] )
-    ZO1 = np.array ( [ [zO1, 0.0, 0.0], [ 0.0, zO2, 0.0 ], [ 0.0, 0.0, zO2] ] )
-    ZO2 = np.array ( [ [zO2, 0.0, 0.0], [ 0.0, zO1, 0.0 ], [ 0.0, 0.0, zO2] ] )
-    ZO3 = np.array ( [ [zO2, 0.0, 0.0], [ 0.0, zO2, 0.0 ], [ 0.0, 0.0, zO1] ] )
-    # Read atomic displacements of Ba, Ti, O1, O2, and O3 as a numpy array
-    natoms = 10 *(n**3)
-    P = np.array([0., 0., 0.])
-    if filename =="displdump":
-        srstr = "c_dr"
-    else:
-        srstr = "f_AVE_ATOM"
-    for a in range(0, natoms, 10):
-        drBa = np.asarray( list( itertools.chain.from_iterable( d.atom(a+1,\
-                       "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr) ) ) )
-        drTi = np.asarray( list( itertools.chain.from_iterable( d.atom(a+3,\
-                       "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr) ) ) )
-        drO1 = np.asarray( list( itertools.chain.from_iterable( d.atom(a+5,\
-                       "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr) ) ) )
-        drO2 = np.asarray( list( itertools.chain.from_iterable( d.atom(a+7,\
-                       "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr) ) ) )
-        drO3 = np.asarray( list( itertools.chain.from_iterable( d.atom(a+9,\
-                       "%s[1]"%srstr, "%s[2]"%srstr, "%s[3]"%srstr) ) ) )
-
-        print "Atomic Displacements of Ba, Ti, O1, O2, and O3: \n", drBa, \
-                "\n", drTi, "\n", drO1, "\n", drO2, "\n", drO3 
-
-        p= np.dot( ZBa, drBa ) + np.dot( ZTi, drTi ) + np.dot( ZO1, drO1) \
-          +  np.dot( ZO2, drO2) + np.dot( ZO3, drO3)
-        P += 1.60217662 * p * 10.0 / ( 64.0 * n**3 )
-    if P[0] < 0. and P[1] < 0. and P[2] < 0.:
-        f = -1.
-    else:
-        f = 1.  
-    print "The absolute polarization: ", (np.append( P, [ f*la.norm(P)] )).tolist()
-    return (np.append( P, [ f*la.norm(P)] )).tolist()
-
-"""
