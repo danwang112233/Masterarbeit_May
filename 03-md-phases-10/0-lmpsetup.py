7e7843894db29f2b9c288b88f154a3ab990f78b7
danwang112233 <wmlvbm613@gmail.com>
2017-06-24 00:05:06 +0200

    Update 0-lmpsetup.py

--- 03-md-phases-10/0-lmpsetup.py
+++ 03-md-phases-10/0-lmpsetup.py
@@ -132,8 +132,8 @@ for T in rng_T:
         step = lmp.extract_variable( "STEP", "all", 0 )
         temp = float(find_temp( step, f_ave_temp))
         lat  = find_lat( step, f_ave_lat)
-        POL = np.vstack((POL, np.asarray([temp]+pol("displcoredump", a, alpha, n) + \
-              [float(lat[0])/n,float(lat[1])/n,float(lat[2])/n ])))
+        POL = np.vstack((POL, np.asarray([temp]+pol("displcoredump", a, alpha, nx, ny, nz) + \
+              [float(lat[0])/nx,float(lat[1])/ny,float(lat[2])/nz ])))
         print POL
 if rank == 0:
     with open("%d_pol_%d_%.2f_%.2f.dat" %(n, DT, Tdamp, Pdamp), 'w') as file_handle:
